e8a44b45e625da7929b3de96f17876879e6a9ac9
Swapnajit Swain <swapnajitswain2003@gmail.com>
2025-11-14 19:25:39 +0530

    React Basic

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Chai from "./chai"
+
+
+function App() {
+ const username = "chai aur code"
+
+  return (
+    <>
+   <Chai/>
+   <h1>Chai Aur React{username}</h1>
+   <p>Test Para</p>
+   </>
+  )
+}
+
+export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  // let counter = 5
+
+  const addValue = () => {
+    //  counter = counter + 1
+    setCounter(counter + 1);
+    // console.log("Clicked", counter)
+    if (counter == 100) setCounter(counter); // It adjusts that the counter value shold not be greater than 100.
+  };
+
+  const removeValue = () => {
+    //  counter = counter - 1
+    setCounter(counter - 1);
+    // console.log("Clicked", counter)
+    if (counter < 1) setCounter(counter); // It adjusts that the counter value shold not be negative.
+  };
+
+  //   const addValue = () => {
+  //   setCounter(prevCounter => (prevCounter < 100 ? prevCounter + 1 : 100));
+  // };
+
+  // const removeValue = () => {
+  //   setCounter(prevCounter => (prevCounter > 0 ? prevCounter - 1 : 0));
+  // };
+
+
+  return (
+    <>
+      <h1>Chai Aur React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value: {counter}</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value: {counter}</button>
+    </>
+  );
+}
+
+export default App;
